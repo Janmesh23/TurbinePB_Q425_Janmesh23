9c7f343b7b64ac324566deccc8728b64097acc98
JANMESH ARUN SHEWALE <worksjanmesh@gmail.com>
2025-10-24 19:32:10 +0530

    anchor counter program done

--- adv-task1/anchor_counter_program/programs/anchor_counter_program/src/lib.rs
+++ adv-task1/anchor_counter_program/programs/anchor_counter_program/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("3Qvio5Ky2VN2vBdyeo3263GZpQHhGN9bEFkNJxLBXa5S");
+declare_id!("EzMqJLtTVK3NTZW4qXEaVHTkbk5E6abEMXComuZwo2Qo");
 
 #[program]
 pub mod anchor_counter_program {
